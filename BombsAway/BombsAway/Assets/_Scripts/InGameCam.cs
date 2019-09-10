3f0619f8a3facf9d321f3753230288c08d6ab218
Shaun Prince <prince.a.shaun@gmail.com>
2019-09-09 17:44:49 -0700

    Initial attempt at multicam scaling code

---
+++ BombsAway/BombsAway/Assets/_Scripts/InGameCam.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameCam : MonoBehaviour
+{
+    public int xCord;
+    public int yCord;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ BombsAway/BombsAway/Assets/_Scripts/InGameCameraManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameCameraManager : MonoBehaviour
+{
+    //   public enum ECamScale { small, even, center }; 
+    // public ECamScale chosenCamScale;
+    public float chosenCamScale;
+    public float smallCamScale;
+    public float smallCamOffset;
+    public float evenCamScale;
+    public float evenCamOffset;
+    public float CenterCamScale;
+    public float CenterCamOffset;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetCameraScales(chosenCamScale);
+    }
+
+    public void SetCameraScales(float newScale)
+    {
+        foreach (Camera c in this.gameObject.transform.GetComponentsInChildren<Camera>())
+        {
+            // calculate the offset based on the chosen cam scale
+            c.rect = new Rect((.5f - chosenCamScale/2) * c.GetComponent<InGameCam>().xCord, (.5f - chosenCamScale/2)
+                * c.GetComponent<InGameCam>().yCord, chosenCamScale, chosenCamScale);
+
+        }
+    }
+}
