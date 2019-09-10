89f22644d84728dbd8dccbc9ec5f495012f3eac4
Shaun Prince <prince.a.shaun@gmail.com>
2019-09-10 11:05:41 -0700

    Functional split camera, Fixed radar/map/schematic cams, functional blackout pannel for in use camera

--- BombsAway/BombsAway/Assets/_Scripts/InGameCameraManager.cs
+++ BombsAway/BombsAway/Assets/_Scripts/InGameCameraManager.cs
@@ -11,28 +11,43 @@ public class InGameCameraManager : MonoBehaviour
     public float smallCamOffset;
     public float evenCamScale;
     public float evenCamOffset;
-    public float CenterCamScale;
-    public float CenterCamOffset;
+    public float centerCamScale;
+    public float centerCamOffset;
+    public Camera[] cams;
+    public Camera activeCenterCam;
+    public RectTransform blackoutPannelRectTran;
+    public RectTransform canvasRectTran;
+    private float canvasWidth;
+    private float canvasHeight;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        canvasWidth = canvasRectTran.rect.width;
+        canvasHeight = canvasRectTran.rect.height;
     }
 
     // Update is called once per frame
     void Update()
     {
         SetCameraScales(chosenCamScale);
+        Debug.Log(canvasRectTran.rect.width);
+        blackoutPannelRectTran.anchoredPosition = new Vector3(activeCenterCam.rect.x * canvasWidth, activeCenterCam.rect.y * canvasHeight, 0f);
+        blackoutPannelRectTran.sizeDelta = new Vector2(activeCenterCam.rect.width * canvasWidth, activeCenterCam.rect.height * canvasHeight);
+        //blackoutPannelRectTran.localScale = new Vector3(activeCenterCam.rect.)
+
+        activeCenterCam.rect = new Rect(0, 0, 1, 1);
+        activeCenterCam.depth = 0;
     }
 
     public void SetCameraScales(float newScale)
     {
-        foreach (Camera c in this.gameObject.transform.GetComponentsInChildren<Camera>())
+        foreach (Camera c in cams)
         {
             // calculate the offset based on the chosen cam scale
             c.rect = new Rect((.5f - chosenCamScale/2) * c.GetComponent<InGameCam>().xCord, (.5f - chosenCamScale/2)
                 * c.GetComponent<InGameCam>().yCord, chosenCamScale, chosenCamScale);
+            c.depth = 1;
 
         }
     }
