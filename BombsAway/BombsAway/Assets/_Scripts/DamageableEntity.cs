6d87d02055ef6fb2323e63b49b74154c762ba52e
Shaun Prince <prince.a.shaun@gmail.com>
2019-09-30 16:09:38 -0700

    Failed attempt at camera snapshot, initial flight and plane damage scripting started

---
+++ BombsAway/BombsAway/Assets/_Scripts/DamageableEntity.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageableEntity : MonoBehaviour
+{
+    public float health;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float incomingDamage)
+    {
+        health -= incomingDamage;
+        if(health <= 0)
+        {
+            GameObject.Destroy(this);
+        }
+    }
+}
