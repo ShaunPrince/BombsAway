9adef8cb9bd6e9712ccea3605b0428c62a8d4011
Shaun <prince.a.shaun@gmail.com>
2019-10-03 15:02:26 -0700

    Initial control schemes, blanks mostly, hopefuly going to refine the selection system of both cameras and control schemes

--- BombsAway/BombsAway/Assets/_Scripts/PlayerControlScripts/CamSelectControls.cs
+++ BombsAway/BombsAway/Assets/_Scripts/PlayerControlScripts/CamSelectControls.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CamSelectControls : MonoBehaviour
 {
     public InGameCameraManager inGameCamManager;
+
     // Start is called before the first frame update
     void Start()
     {
