6b57ec193aa7d8772682752c39fd5e05e9a7c4fb
Shaun <prince.a.shaun@gmail.com>
2019-10-02 18:32:49 -0700

    More polished flying script, altitude, speed, and rotation

--- BombsAway/BombsAway/Assets/_Scripts/Flying.cs
+++ BombsAway/BombsAway/Assets/_Scripts/Flying.cs
@@ -16,6 +16,8 @@ public class Flying : MonoBehaviour
     public float desiredDir;
     public float turningRate;
 
+    public float lateralMovementDamperScale;
+
     public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
@@ -35,19 +37,31 @@ public class Flying : MonoBehaviour
     {
 
         currentAltitude = this.transform.position.y;
-        //ser vertical velocity to the velocity per second (Acceleration)
+        //set vertical velocity to the current velocity plus/minus the velocity per second (Acceleration)
         rb.velocity = new Vector3(rb.velocity.x,Mathf.Sign(desireAltitude - currentAltitude) * verticalAcceleration * Time.deltaTime,rb.velocity.z);
         
+        //add forward/backward force to reach desired speed
         currentForwardSpeed = this.transform.InverseTransformVector(rb.velocity).z;
         rb.AddForce(this.transform.forward * Mathf.Sign(desiredForwardSpeed - currentForwardSpeed) * forwardAcceleration * Time.deltaTime, ForceMode.VelocityChange);
-    
-        rb.MoveRotation(Quaternion.Euler())
+
+        //Dampen lateral movement, aka drifting during turns
+        rb.AddRelativeForce(-this.transform.InverseTransformVector(rb.velocity).x/lateralMovementDamperScale, 0f, 0f, ForceMode.VelocityChange);
+
+        //Debug.Log(this.transform.InverseTransformDirection(rb.velocity).x);
+        
+        //apply rotation toward desired rotation
+        rb.MoveRotation(Quaternion.Euler(0f, calculateTurn(), 0f));
     }
 
-    private void calculateTurn()
+    private float calculateTurn()
     {
         //set abs value of direction between 0 and 306
         currentDir = this.transform.rotation.eulerAngles.y % 360;
+
+        if(Mathf.Abs(desiredDir - currentDir) < turningRate*Time.deltaTime)
+        {
+            return currentDir;
+        }
 
         //convert -dir to their positive equivalent
         if(currentDir < 0)
@@ -60,12 +74,30 @@ public class Flying : MonoBehaviour
         //needs to turn right
         if(currentDir < desiredDir)
         {
-            deltaDir = desiredDir - currentDir;
+            if(desiredDir - currentDir <= 180)
+            {
+                deltaDir = desiredDir - currentDir;
+            }
+            else
+            {
+                deltaDir = currentDir - desiredDir;
+            }
+
         }
+        //left
         else
         {
-            deltaDir = currentDir - desiredDir;
+            if(currentDir - desiredDir <= 180)
+            {
+                deltaDir = desiredDir - currentDir;
+            }
+            else
+            {
+                deltaDir = currentDir - desiredDir;
+            }
         }
+
+        return  currentDir + (Mathf.Sign(deltaDir) * turningRate * Time.deltaTime);
 
 
     }
