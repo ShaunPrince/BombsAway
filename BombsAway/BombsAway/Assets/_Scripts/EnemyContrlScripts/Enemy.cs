bb17d9f3ac610f304109076feeb5a6b0b44be2f0
aderevlany <avaderevlany@gmail.com>
2019-10-07 16:32:20 -0700

    Started on Enemy scripts in a new scene

---
+++ BombsAway/BombsAway/Assets/_Scripts/EnemyContrlScripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * scrip takes care of the randomness - so we dont have to pass variables
+simple enemy command script
+
+random spawn
+altitude height of player
+direction to player from enemy - const update
+desired speed (random)
+
+gets to close? go over and fly away and turn around a little bit later to re-attack
+
+damageable entity - script
+take damage - health stuffs
+
+just fly towards player for now
+
+use shaun's script - flying script
+
+*/
+
+public class Enemy : DamageableEntity
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ BombsAway/BombsAway/Assets/_Scripts/EnemyContrlScripts/EnemySpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * 
+scrip takes care of the randomness - so we dont have to pass variables
+simple enemy command script
+
+random spawn
+altitude height of player
+direction to player from enemy - const update
+desired speed (random)
+
+gets to close? go over and fly away and turn around a little bit later to re-attack
+
+damageable entity - script
+take damage - health stuffs
+
+just fly towards player for now
+
+use shaun's script - flying script
+
+*/
+
+public class EnemySpawner : MonoBehaviour
+{
+    public float worldSize;     // change later to get from world script
+    public EnemySpawn[] Enemies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public class EnemySpawn {
+
+    public GameObject EnemyPrefab;
+    public float probabilityOfSpawn;
+    // speed, health?
+}
