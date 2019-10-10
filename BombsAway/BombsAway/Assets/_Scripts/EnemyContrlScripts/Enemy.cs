85668122e8ec1260064fdd0621f56e93fedadcf8
aderevlany <avaderevlany@gmail.com>
2019-10-10 14:29:48 -0700

    Started on EnemySpawning
    Enemies have a probability of being spawned and individual healths/speeds

--- BombsAway/BombsAway/Assets/_Scripts/EnemyContrlScripts/Enemy.cs
+++ BombsAway/BombsAway/Assets/_Scripts/EnemyContrlScripts/Enemy.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /*
- * scrip takes care of the randomness - so we dont have to pass variables
+ * 
+scrip takes care of the randomness - so we dont have to pass variables
 simple enemy command script
 
 random spawn
@@ -22,17 +23,25 @@ use shaun's script - flying script
 
 */
 
-public class EnemyUnit : DamageableEntity
+public class Enemy : DamageableEntity
 {
+    public int speed;
+    [Range(0, 1)]
+    public float percision; // in terms of flying towards player, altitude relative to player
+                            // don't want perfect aim
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // constantly update location to which to fly to (location of player)
+        // constantly update altitude until it is within a certain range of the player?
+        // when enemy gets too close to player, raise up, fly over, fly forward for a short time, then return to attacking
+
     }
 }
