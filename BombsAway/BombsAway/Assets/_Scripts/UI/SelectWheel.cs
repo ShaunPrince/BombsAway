530a6e447e53d84bea9694ad65a60bf683c9b4e0
Victoria B <vbarinova97@gmail.com>
2019-10-14 20:37:45 -0700

    Wheel is Selectable
    Basically, drop the canvas prefab, it will activate when holding Tab and will rotate the pointer accordingly BUT it will disappear in the middle since middle selection is not supposed to exist. Release Tab and it will disappear.

---
+++ BombsAway/BombsAway/Assets/_Scripts/UI/SelectWheel.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SelectWheel : MonoBehaviour
+{
+
+    private GraphicRaycaster ray;
+    private PointerEventData PointerEventData;
+    private EventSystem EventSystem;
+
+    public GameObject wheel;
+    public GameObject pointer;
+    public GameObject element;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ray = GetComponent<GraphicRaycaster>();
+        EventSystem = GetComponent<EventSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Tab)) {
+            wheel.SetActive(true);
+            element.SetActive(true);
+            PointerEventData = new PointerEventData(EventSystem);
+            PointerEventData.position = Input.mousePosition;
+
+            List<RaycastResult> result = new List<RaycastResult>();
+
+            ray.Raycast(PointerEventData, result);
+            if (result.ToArray().Length != 0)
+            {
+                //Debug.Log(result[0]);
+                var pos = result[0].screenPosition;
+                if (result[0].gameObject.tag != "Finish")
+                {
+                    pointer.SetActive(true);
+                    float angle = -1 * Mathf.Atan2(pos.x - Screen.width / 2, pos.y - Screen.height / 2) * Mathf.Rad2Deg;
+                    //Debug.Log(angle);
+                    wheel.transform.rotation = Quaternion.Euler(0, 0, angle);
+                }
+                else
+                {
+                    pointer.SetActive(false);
+                }
+
+            }
+
+
+        }
+        else
+            if (wheel.activeSelf) { wheel.SetActive(false); element.SetActive(false); }
+
+
+    }
+}
