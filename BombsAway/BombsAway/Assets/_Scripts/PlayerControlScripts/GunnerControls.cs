52a32fc4353bab8e01f0133f9a15fc764d88a321
gavindent <gavindent@hotmail.com>
2019-10-22 00:53:50 -0700

    Angle lock working for gun cameras. GunController camera script updated.

--- BombsAway/BombsAway/Assets/_Scripts/PlayerControlScripts/GunnerControls.cs
+++ BombsAway/BombsAway/Assets/_Scripts/PlayerControlScripts/GunnerControls.cs
@@ -58,28 +58,24 @@ public class GunnerControls : ControlScheme
         float deltaX = Input.GetAxis("Mouse X");
         float deltaY = Input.GetAxis("Mouse Y");
 
-        //Debug.Log(myCamera.transform.localRotation.eulerAngles.x + deltaY * lookSensitivity);
         //Rotation about x yields a up down change
-        if (Mathf.Abs(startingXRotation - myCamera.transform.localRotation.eulerAngles.x + -deltaY * lookSensitivity) <= maxXLook &&
-            -Mathf.Abs(startingXRotation - myCamera.transform.localRotation.eulerAngles.x + -deltaY * lookSensitivity) >= minXLook)
+        if (myCamera.transform.localRotation.eulerAngles.x + -deltaY * lookSensitivity <= maxXLook ||
+            myCamera.transform.localRotation.eulerAngles.x + -deltaY * lookSensitivity >= minXLook)
         {
-
             myCamera.transform.Rotate(-deltaY * lookSensitivity, 0f, 0f);
         }
-
         //Go back later and force constraints
-        myCamera.transform.Rotate(-deltaY * lookSensitivity, 0f, 0f);
+        //myCamera.transform.Rotate(-deltaY * lookSensitivity, 0f, 0f);
 
-
-        if (Mathf.Abs(startingYRotation - this.transform.parent.localRotation.eulerAngles.y + deltaX * lookSensitivity) <= maxYLook &&
-            -Mathf.Abs(startingYRotation - this.transform.parent.localRotation.eulerAngles.y + deltaX * lookSensitivity) >= minYLook)
+        if (this.transform.parent.localRotation.eulerAngles.y + deltaX * lookSensitivity <= maxYLook ||
+            this.transform.parent.localRotation.eulerAngles.y + deltaX * lookSensitivity >= minYLook)
         {
             this.transform.parent.Rotate(0f, deltaX * lookSensitivity, 0f);
 
         }
 
         //Go back and force constraints
-        this.transform.parent.Rotate(0f, deltaX * lookSensitivity, 0f);
+        //this.transform.parent.Rotate(0f, deltaX * lookSensitivity, 0f);
 
     }
 }
