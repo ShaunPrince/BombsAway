f71c6233f2a9aba21bd27d690861155656bb0b82
Victoria B <vbarinova97@gmail.com>
2019-10-21 22:09:18 -0700

    Simple Main Menu + TextMeshPro
    Enabled TextMeshPro, using it for menu UIs. Also added "Change Cameras" instruction title to the camera selection wheel.
    
    Main Menu: Can edit the controls page by selecting its gameobject and adding on to it.

---
+++ BombsAway/BombsAway/Assets/_Scripts/UI/MainMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    [Header("Attach Controls page related object here")]
+    public GameObject ControlsPage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void LoadLevel(int index)
+    {
+        Debug.Log("Menu Start button launches next scene.");
+        SceneManager.LoadScene(index);
+    }
+
+    public void ViewControls()
+    {
+        if (ControlsPage.activeSelf) ControlsPage.SetActive(false);
+        else ControlsPage.SetActive(true);
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Game is supposed to quit.");
+    }
+
+}
