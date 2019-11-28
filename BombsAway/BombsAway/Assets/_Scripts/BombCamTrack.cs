3ba9f84611c3fcfdae01b5810cccf1609268513c
Shaun Prince <prince.a.shaun@gmail.com>
2019-11-27 18:54:22 -0800

    Made the bomb cam swivvel so that the landing zone is always in view. Increased the spread of bombs, changed the bomb canvas to camera instead of world space as it was appearing on other cams when it swiveled. Still deciding what to do about the bomb placement on screen

---
+++ BombsAway/BombsAway/Assets/_Scripts/BombCamTrack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombCamTrack : MonoBehaviour
+{
+    public Transform impactSpotTF;
+    public LayerMask layersToHit;
+
+    private Vector3 centerOfImpactAreaPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(impactSpotTF.position,Vector3.down, out hit, 10000f,layersToHit))
+        {
+            centerOfImpactAreaPos = hit.point;
+        }
+        Debug.DrawLine(this.transform.position , centerOfImpactAreaPos);
+        this.gameObject.transform.LookAt(centerOfImpactAreaPos);
+    }
+}
