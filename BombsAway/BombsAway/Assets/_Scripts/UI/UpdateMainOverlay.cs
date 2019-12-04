585f7ebbd6c4bab00042776a30e793dfedf403a5
Victoria B <vbarinova97@gmail.com>
2019-12-03 22:53:48 -0800

    Main Overlay!

---
+++ BombsAway/BombsAway/Assets/_Scripts/UI/UpdateMainOverlay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateMainOverlay : MonoBehaviour
+{
+    public GameObject manager;
+    private int value;
+
+    public Sprite[] overlays;
+    private Image self;
+
+    void Awake()
+    {
+        self = this.GetComponent<Image>();
+        //value = (int)manager.GetComponent<StationDisplayPosAndScaleController>().chosenCenterCamScale;
+    }
+
+
+
+    void LateUpdate() {
+        // was gonna have events here but w/e
+        value = (int)manager.GetComponent<StationDisplayPosAndScaleController>().chosenCenterCamScale;
+        UpdateOverlay(value);
+
+    }
+    void UpdateOverlay(int value) {
+        self.sprite = overlays[value];
+    }
+}
