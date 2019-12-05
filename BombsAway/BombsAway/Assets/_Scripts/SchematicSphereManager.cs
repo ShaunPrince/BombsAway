f4139bc5af7d7433fd361b3eaca0702c67636beb
gavindent <gavindent@hotmail.com>
2019-12-04 18:29:54 -0800

    Line renderer changes added to stay with spheres

--- BombsAway/BombsAway/Assets/_Scripts/SchematicSphereManager.cs
+++ BombsAway/BombsAway/Assets/_Scripts/SchematicSphereManager.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class SchematicSphereManager : MonoBehaviour
 {
-    public Material activeColor;
-    public Material inactiveColor;
+    public GameObject leftGun;
+    public GameObject rightGun;
+    public GameObject tailGun;
 
     private List<GameObject> schematicSpheres = new List<GameObject>();
     private int currentStationID;
+    private LineRenderer leftGunLR;
+    private LineRenderer rightGunLR;
+    private LineRenderer tailGunLR;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,20 +23,30 @@ public class SchematicSphereManager : MonoBehaviour
             schematicSpheres[i-1].GetComponent<Renderer>().material.SetColor("_BaseColor", Color.red);
         }
         currentStationID = 0;
+        leftGunLR = schematicSpheres[3].GetComponent<LineRenderer>();
+        rightGunLR = schematicSpheres[4].GetComponent<LineRenderer>();
+        tailGunLR = schematicSpheres[6].GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SetLeftGunAim();
     }
 
     public void SetNewActiveStation(int newStationID)
     {
-        //GameObject currentActiveSphere = schematicSpheres[currentStationID];
-        //GameObject newActiveSphere = schematicSpheres[newStationID-1];
-        //schematicSpheres[currentStationID].GetComponent<Renderer>().material.SetColor("_BaseColor", Color.red);
-        //schematicSpheres[newStationID-1].GetComponent<Renderer>().material.SetColor("_BaseColor", Color.green);
+        schematicSpheres[currentStationID].GetComponent<Renderer>().material.SetColor("_BaseColor", Color.red);
+        schematicSpheres[newStationID-1].GetComponent<Renderer>().material.SetColor("_BaseColor", Color.green);
         currentStationID = newStationID - 1;
+    }
+
+    private void SetLeftGunAim()
+    {
+        GameObject leftGunCircle = schematicSpheres[3];
+        Vector3 linePos = new Vector3(leftGunCircle.transform.position.x, leftGunCircle.transform.position.y + 0.01f, leftGunCircle.transform.position.z);
+
+        leftGunLR.SetPosition(0, linePos);
+        leftGunLR.SetPosition(1, leftGunCircle.transform.right * -0.5f + linePos);
     }
 }
