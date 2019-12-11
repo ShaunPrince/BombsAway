ff5a9852e16c96bc61b954c389b6a3134a16228c
gavindent <gavindent@hotmail.com>
2019-12-11 00:47:03 -0800

    Added child objects to schematic spheres to show a second line to indicate the camera FOV for the gun cameras.

--- BombsAway/BombsAway/Assets/_Scripts/SchematicSphereManager.cs
+++ BombsAway/BombsAway/Assets/_Scripts/SchematicSphereManager.cs
@@ -10,9 +10,12 @@ public class SchematicSphereManager : MonoBehaviour
 
     private List<GameObject> schematicSpheres = new List<GameObject>();
     private int currentStationID;
-    private LineRenderer leftGunLR;
-    private LineRenderer rightGunLR;
-    private LineRenderer tailGunLR;
+    private LineRenderer leftGunLR1;
+    private LineRenderer leftGunLR2;
+    private LineRenderer rightGunLR1;
+    private LineRenderer rightGunLR2;
+    private LineRenderer tailGunLR1;
+    private LineRenderer tailGunLR2;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,9 +26,12 @@ public class SchematicSphereManager : MonoBehaviour
             schematicSpheres[i-1].GetComponent<Renderer>().material.SetColor("_BaseColor", Color.red);
         }
         currentStationID = 0;
-        leftGunLR = schematicSpheres[3].GetComponent<LineRenderer>();
-        rightGunLR = schematicSpheres[4].GetComponent<LineRenderer>();
-        tailGunLR = schematicSpheres[6].GetComponent<LineRenderer>();
+        leftGunLR1 = schematicSpheres[3].GetComponent<LineRenderer>();
+        leftGunLR2 = schematicSpheres[3].transform.GetChild(0).GetComponent<LineRenderer>();
+        rightGunLR1 = schematicSpheres[4].GetComponent<LineRenderer>();
+        rightGunLR2 = schematicSpheres[4].transform.GetChild(0).GetComponent<LineRenderer>();
+        tailGunLR1 = schematicSpheres[6].GetComponent<LineRenderer>();
+        tailGunLR2 = schematicSpheres[6].transform.GetChild(0).GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
@@ -45,28 +51,46 @@ public class SchematicSphereManager : MonoBehaviour
 
     private void SetLeftGunAim()
     {
-        GameObject leftGunCircle = schematicSpheres[3];
-        Vector3 linePos = leftGunCircle.transform.position;
-        leftGunCircle.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, -leftGun.transform.localRotation.eulerAngles.y);
-        leftGunLR.SetPosition(0, linePos);
-        leftGunLR.SetPosition(1, leftGunCircle.transform.right * -0.5f + linePos);
+        GameObject leftGunCircle1 = schematicSpheres[3];
+        GameObject leftGunCircle2 = leftGunCircle1.transform.GetChild(0).gameObject;
+        Vector3 linePos1 = leftGunCircle1.transform.position;
+        Vector3 linePos2 = leftGunCircle2.transform.position;
+        leftGunCircle1.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, -leftGun.transform.localRotation.eulerAngles.y - 30.0f);
+        leftGunCircle2.transform.localRotation = Quaternion.identity;
+        leftGunCircle2.transform.Rotate(0.0f, 0.0f, 60.0f);
+        leftGunLR1.SetPosition(0, linePos1);
+        leftGunLR1.SetPosition(1, leftGunCircle1.transform.right * -0.5f + linePos1);
+        leftGunLR2.SetPosition(0, linePos2);
+        leftGunLR2.SetPosition(1, leftGunCircle2.transform.right * -0.5f + linePos2);
     }
 
     private void SetRightGunAim()
     {
-        GameObject rightGunCircle = schematicSpheres[4];
-        Vector3 linePos = rightGunCircle.transform.position;
-        rightGunCircle.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, -rightGun.transform.localRotation.eulerAngles.y);
-        rightGunLR.SetPosition(0, linePos);
-        rightGunLR.SetPosition(1, (rightGunCircle.transform.right * 0.5f) + linePos);
+        GameObject rightGunCircle1 = schematicSpheres[4];
+        GameObject rightGunCircle2 = rightGunCircle1.transform.GetChild(0).gameObject;
+        Vector3 linePos1 = rightGunCircle1.transform.position;
+        Vector3 linePos2 = rightGunCircle2.transform.position;
+        rightGunCircle1.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, -rightGun.transform.localRotation.eulerAngles.y - 30.0f);
+        rightGunCircle2.transform.localRotation = Quaternion.identity;
+        rightGunCircle2.transform.Rotate(0.0f, 0.0f, 60.0f);
+        rightGunLR1.SetPosition(0, linePos1);
+        rightGunLR1.SetPosition(1, (rightGunCircle1.transform.right * 0.5f) + linePos1);
+        rightGunLR2.SetPosition(0, linePos2);
+        rightGunLR2.SetPosition(1, (rightGunCircle2.transform.right * 0.5f) + linePos2);
     }
 
     private void SetTailGunAim()
     {
-        GameObject tailGunCircle = schematicSpheres[6];
-        Vector3 linePos = tailGunCircle.transform.position;
-        tailGunCircle.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, -tailGun.transform.localRotation.eulerAngles.y);
-        tailGunLR.SetPosition(0, linePos);
-        tailGunLR.SetPosition(1, (tailGunCircle.transform.up * -0.5f) + linePos);
+        GameObject tailGunCircle1 = schematicSpheres[6];
+        GameObject tailGunCircle2 = tailGunCircle1.transform.GetChild(0).gameObject;
+        Vector3 linePos1 = tailGunCircle1.transform.position;
+        Vector3 linePos2 = tailGunCircle2.transform.position;
+        tailGunCircle1.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, -tailGun.transform.localRotation.eulerAngles.y - 30.0f);
+        tailGunCircle2.transform.localRotation = Quaternion.identity;
+        tailGunCircle2.transform.Rotate(0.0f, 0.0f, 60.0f);
+        tailGunLR1.SetPosition(0, linePos1);
+        tailGunLR1.SetPosition(1, (tailGunCircle1.transform.up * -0.5f) + linePos1);
+        tailGunLR2.SetPosition(0, linePos2);
+        tailGunLR2.SetPosition(1, (tailGunCircle2.transform.up * -0.5f) + linePos2);
     }
 }
