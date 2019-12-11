73efa44708a3d64f63454652de50f830176ac133
Victoria B <vbarinova97@gmail.com>
2019-12-10 17:49:50 -0800

    Radar rotation swipe should match the beam
    But longer I play more I notice a lot of false positives that show up on the radar that are out of sync? absolutely no problem when it is just 1 ship on screen and rotation works fine

--- BombsAway/BombsAway/Assets/_Scripts/Stations/Radar/RotateRadar.cs
+++ BombsAway/BombsAway/Assets/_Scripts/Stations/Radar/RotateRadar.cs
@@ -1,17 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RotateRadar : MonoBehaviour
 {
     public float secondsPerRotation;
     private Rigidbody rb;
+    //private Image sprite;
 
     public GameObject radarswipe;
+    public GameObject parentTransform;
+    private Vector3 oldparentTransform;
     // Start is called before the first frame update
     void Awake()
     {
         rb = this.GetComponentInChildren<Rigidbody>();
+        //sprite = radarswipe.GetComponent<Image>();
+        oldparentTransform = parentTransform.transform.rotation.eulerAngles;
     }
 
     // Update is called once per frame
@@ -22,12 +28,23 @@ public class RotateRadar : MonoBehaviour
 
     private void FixedUpdate()
     {
+        //figuring out how much the parent rotated delta to offset the radar swipe on z axis
+        Vector3 newAngle = oldparentTransform - parentTransform.transform.rotation.eulerAngles;
+        newAngle = new Vector3(newAngle.x, newAngle.z, newAngle.y);
+
+        // radar beamer
         Vector3 curRotation = rb.transform.rotation.eulerAngles;
         Vector3 newRotation = curRotation + new Vector3(0, 360 / (secondsPerRotation * 50), 0);
-        
         rb.MoveRotation(Quaternion.Euler(newRotation));
 
-        Vector3 newSRotation = radarswipe.transform.rotation.eulerAngles + new Vector3(0, 0, -1* 360 / (secondsPerRotation * 50));
+        //radar swipe
+        Vector3 newSRotation = radarswipe.transform.rotation.eulerAngles -
+                newAngle + new Vector3(0, 0, ( 360 / (secondsPerRotation * 50))*-1);
         radarswipe.transform.SetPositionAndRotation(radarswipe.transform.position, Quaternion.Euler(newSRotation));
+
+        if (oldparentTransform != parentTransform.transform.rotation.eulerAngles)
+        {
+            oldparentTransform = parentTransform.transform.rotation.eulerAngles;
+        }
     }
 }
