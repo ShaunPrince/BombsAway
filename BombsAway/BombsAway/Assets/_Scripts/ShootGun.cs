1c39499825b33346865b53ef02b78278b8d4fe44
aderevlany <avaderevlany@gmail.com>
2020-01-14 16:32:51 -0800

    Added a tranform that all the bullets should spawn under, so they do not fill the inspector hierarchy.

--- BombsAway/BombsAway/Assets/_Scripts/ShootGun.cs
+++ BombsAway/BombsAway/Assets/_Scripts/ShootGun.cs
@@ -7,11 +7,12 @@ public class ShootGun : MonoBehaviour
     public GameObject projectile;
     public float projectileSpeed;
     public float gunDamage;
+    public Transform bulletOrganizationTransform;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-
+        bulletOrganizationTransform = GameObject.FindGameObjectWithTag("BulletTransformStorage").transform;
     }
 
     // Update is called once per frame
@@ -28,5 +29,9 @@ public class ShootGun : MonoBehaviour
         newBC.allegiance = this.transform.GetComponentInParent<DamageableEntity>().allegiance;
         newBC.SetBulletDamage(gunDamage);
         newProjectile.GetComponent<Rigidbody>().velocity = this.GetComponentInParent<Rigidbody>().velocity + this.GetComponentInParent<Camera>().gameObject.transform.forward * projectileSpeed;
+        if(bulletOrganizationTransform != null)
+        {
+            newProjectile.transform.SetParent(bulletOrganizationTransform);
+        }
     }
 }
