3273d6450c949aa722ac7b3006641140e2f4eb6a
gavindent <gavindent@hotmail.com>
2020-01-27 15:06:11 -0800

    Added sensitivity slider to options menu. Adjusts mouse senstivity for the gunner stations based on value of slider. Slider begins at 25 percent.

---
+++ BombsAway/BombsAway/Assets/_Scripts/LookSensitivityController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LookSensitivityController : MonoBehaviour
+{
+    public Slider slider;
+    public GameObject leftGunStationControls;
+    public GameObject rightGunStationControls;
+    public GameObject tailGunStationControls;
+
+    private float mouseSens;
+    private GunnerControls lControls;
+    private GunnerControls rControls;
+    private GunnerControls tControls;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.value = 0.25f;
+        mouseSens = 1.0f;
+        lControls = leftGunStationControls.GetComponent<GunnerControls>();
+        rControls = rightGunStationControls.GetComponent<GunnerControls>();
+        tControls = tailGunStationControls.GetComponent<GunnerControls>();
+        SetLookSensitivity();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetLookSensitivity()
+    {
+        if (lControls == null)
+        {
+            return;
+        }
+        float sliderVal = slider.value;
+        mouseSens = Mathf.Pow(10.0f, sliderVal);
+        lControls.lookSensitivity = mouseSens;
+        rControls.lookSensitivity = mouseSens;
+        tControls.lookSensitivity = mouseSens;
+    }
+}
