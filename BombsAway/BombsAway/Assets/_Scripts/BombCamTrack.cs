86db2195786dcac3c7be98d558dec5b8de5bb2e2
aderevlany <avaderevlany@gmail.com>
2020-01-29 18:49:59 -0800

    Added transparent cylinder to be a temp bomb scope, needs work

--- BombsAway/BombsAway/Assets/_Scripts/BombCamTrack.cs
+++ BombsAway/BombsAway/Assets/_Scripts/BombCamTrack.cs
@@ -6,6 +6,9 @@ public class BombCamTrack : MonoBehaviour
 {
     public Transform impactSpotTF;
     public LayerMask layersToHit;
+    public BombDropController bdc;
+    public Transform cylinder;
+    public BombAreaParent bap;
 
     private Vector3 centerOfImpactAreaPos;
     // Start is called before the first frame update
@@ -15,7 +18,7 @@ public class BombCamTrack : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         RaycastHit hit;
         if(Physics.Raycast(impactSpotTF.position,Vector3.down, out hit, 10000f,layersToHit))
@@ -23,6 +26,31 @@ public class BombCamTrack : MonoBehaviour
             centerOfImpactAreaPos = hit.point;
         }
         Debug.DrawLine(this.transform.position , centerOfImpactAreaPos);
+        
+        float theta1 = Vector3.Angle(-this.transform.parent.up*100000, hit.point);
+        Debug.DrawRay(this.transform.position, -this.transform.parent.up * 100,Color.green);
+        float theta2 = Vector3.Angle(-this.transform.parent.up *100000, hit.point + this.transform.parent.TransformVector(new Vector3(0, 0, -bap.radius)));
+        //Debug.Log("tvect: " +this.transform.parent.TransformVector(0, 0, -bap.radius));
+        //Debug.Log("hit+-tvect: " + (hit.point + this.transform.parent.TransformVector(0,0, -bap.radius)));
+        Debug.DrawLine(this.transform.parent.position, hit.point + this.transform.parent.TransformVector(0, 0, -bap.radius), Color.blue);
+
+        float x1 = Vector3.Magnitude(new Vector3(this.transform.parent.position.x,hit.point.y,this.transform.parent.position.z)
+                    - hit.point);
+        float x2 = x1 - bap.radius;
+        float y1 = this.transform.position.y - hit.point.y;
+        theta1 = Mathf.Rad2Deg * Mathf.Atan(x1 / y1);
+        theta2 = Mathf.Rad2Deg * Mathf.Atan(x2 / y1);
+        float y2 = 1/(Mathf.Tan(Mathf.Deg2Rad*theta1) - Mathf.Tan(Mathf.Deg2Rad*theta2));
+        Debug.Log(hit.point);
+        Debug.Log("Radius: " + bap.radius);
+        Debug.Log("x1: " + x1);
+        Debug.Log("Theta 1: " + theta1);
+        Debug.Log(Mathf.Rad2Deg*Mathf.Atan(x1 / y1));
+        Debug.Log("Theta 2: " + theta2);
+        Debug.Log("y1: " + y1);
+        Debug.Log("y2: " + y2);
+        cylinder.transform.localPosition = Vector3.zero;
+        cylinder.transform.localPosition = new Vector3(0, -y2,(Mathf.Tan(Mathf.Deg2Rad*theta2)*y2)+1);
         this.gameObject.transform.LookAt(centerOfImpactAreaPos);
     }
 }
