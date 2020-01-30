0175c030d6fdded08903328fa1000a02d6f09079
aderevlany <avaderevlany@gmail.com>
2020-01-29 21:12:25 -0800

    Added elements for showing the player dying at the end
    Explosion and rotating camera that shows player script
    However, will implement tomorrow

---
+++ BombsAway/BombsAway/Assets/_Scripts/PlayerEndGameDeath.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEndGameDeath : MonoBehaviour
+{
+    public GameObject deathCamObject;
+    public GameObject deathTexture;
+    public GameObject explosionFX;
+    private GameObject player;
+
+    private bool hasDied = false;
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        player = GameObject.Find("PlayerPlane");
+        deathCamObject.SetActive(false);
+    }
+
+    public void ShowPlayerDying()
+    {
+        if (!hasDied)
+        {
+            // explode
+            GameObject explosion = Instantiate(explosionFX, player.transform.position, player.transform.rotation);
+
+            // spin the death cam around player
+            deathCamObject.SetActive(true);
+            iTween.RotateBy(deathCamObject, iTween.Hash("amount", new Vector3(0, 1, 0),
+                                                        "time", 50f, "easetype", "linear",
+                                                        "looptype", "loop"));
+
+            // tween from main camera to new death cam
+            deathTexture.SetActive(true);
+            deathTexture.gameObject.AddComponent<CameraTween>();
+            deathTexture.gameObject.GetComponent<CameraTween>().FadeIn();
+
+            hasDied = true;
+        }
+               
+    }
+}
---
+++ BombsAway/BombsAway/Assets/_Scripts/Tweening/GameObjectTween.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameObjectTween : MonoBehaviour
+{
+    private float fadeTime = .5f;
+    private bool currentlyFading = false;   // to deal with fades in the middle of fades
+    // Start is called before the first frame update
+
+    public void FadeOut()
+    {
+        iTween.ValueTo(gameObject, iTween.Hash(
+            "from", 1.0f, "to", 0.0f,
+            "time", fadeTime, "easetype", "easeOutQuad",
+            "onupdate", "setFade", "oncomplete", "SetHidden"));
+
+        //Debug.Log($"Fading out {this.transform.parent.name}");
+    }
+    public void FadeIn()
+    {
+        iTween.ValueTo(gameObject, iTween.Hash(
+            "from", 0f, "to", 1f,
+            "time", fadeTime, "easetype", "easeOutQuad",
+            "onupdate", "setFade"));
+
+        //Debug.Log($"Fading in {this.transform.parent.name}");
+    }
+    private void setFade(float newAlpha)
+    {
+        //foreach (Material mObj in renderer.materials)
+        //{
+        //    mObj.color = new Color(
+        //        mObj.color.r, mObj.color.g,
+        //        mObj.color.b, newAlpha);
+        //}
+
+        Debug.Log($"{this.name} has {this.transform.GetChild(0).transform.childCount} children with last one at alpha {newAlpha}");
+        Color currentColor = this.transform.GetChild(0).transform.GetChild(this.transform.childCount - 1).GetComponent<RawImage>().color;
+        Color changeColor = new Color(currentColor.r, currentColor.g, currentColor.b, newAlpha);
+        this.transform.GetChild(this.transform.childCount - 1).GetComponent<RawImage>().color = changeColor;
+    }
+
+    private void SetHidden()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
