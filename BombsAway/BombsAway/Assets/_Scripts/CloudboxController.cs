67a35ee729481732cc1f02990b05907459354c47
aderevlany <avaderevlany@gmail.com>
2020-02-03 23:47:47 -0800

    There is now fog based on height and fixed missile explosions
    The heigher up the player is, the foggier
    The lower, the less foggy
    Missile explosions happen where the missile was, not the center of the gameobject

---
+++ BombsAway/BombsAway/Assets/_Scripts/CloudboxController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudboxController : MonoBehaviour
+{
+    private Transform player;
+    private float[] densities = { 0.1f, 0.5f, 1f };
+
+    private float densityTime = 5f;
+    private EAlts prevAlt;
+
+    private float prevDensity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = this.transform.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.GetComponentInChildren<PlayerFlightControls>().currentAltSetting != prevAlt)
+        {
+            UpdateCloudBox((int)player.GetComponentInChildren<PlayerFlightControls>().currentAltSetting);
+            prevAlt = player.GetComponentInChildren<PlayerFlightControls>().currentAltSetting;
+        }
+    }
+
+    private void UpdateCloudBox(int index)
+    {
+        iTween.ValueTo(gameObject, iTween.Hash( "from", prevDensity, "to", densities[index],
+                                                "time", densityTime, "easetype", "linear",
+                                                "onupdate", "UpdateDensity"));
+    }
+
+    private void UpdateDensity(float density)
+    {
+        prevDensity = density;
+        this.transform.GetComponent<Renderer>().material.SetFloat("Vector1_4FEBF9CE", density);
+    }
+}
