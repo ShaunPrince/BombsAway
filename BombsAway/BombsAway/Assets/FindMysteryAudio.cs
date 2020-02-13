86a21d48e75aa0c4f5896b528a3c2347e38661cd
gmdent <dreampowers@gmail.com>
2020-02-12 17:42:38 -0800

    Removed the mystery bomb audio file.

---
+++ BombsAway/BombsAway/Assets/FindMysteryAudio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindMysteryAudio : MonoBehaviour
+{
+    private Object[] objectsWithAudioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectsWithAudioSource = Object.FindObjectsOfType<AudioSource>();
+        for (int i = 0; i < objectsWithAudioSource.Length; ++i)
+        {
+            Debug.Log(objectsWithAudioSource[i].name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
