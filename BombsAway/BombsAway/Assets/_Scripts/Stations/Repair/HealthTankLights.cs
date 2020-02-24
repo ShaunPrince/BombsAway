b68d2627a76def83f776909e2dd09294cb384b9a
aderevlany <avaderevlany@gmail.com>
2020-02-24 15:13:55 -0800

    Script for changing tank lights according to health has been created
    Just needs to be implemented

---
+++ BombsAway/BombsAway/Assets/_Scripts/Stations/Repair/HealthTankLights.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthTankLights : MonoBehaviour
+{
+    public Material[] materials;
+    private Material prevMaterial;
+
+    public float timeBetweenFlickers;
+
+    public void SetTankLightMaterial(int index)
+    {
+        if (index >= 2) this.GetComponent<MaterialTweening>().PingPongMaterial(prevMaterial, materials[3], timeBetweenFlickers);
+        else this.GetComponent<MaterialTweening>().MergeMaterial(prevMaterial, materials[index]);
+        prevMaterial = materials[index];
+    }
+
+    private void Start()
+    {
+        prevMaterial = materials[0];
+    }
+
+}
