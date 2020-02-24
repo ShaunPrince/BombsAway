1d4ab217e2d8c010b2e82247146511004fda15f6
gmdent <dreampowers@gmail.com>
2020-02-24 15:33:38 -0800

    Made clank sound play when adjusting volume

--- BombsAway/BombsAway/Assets/_Scripts/Audio/SetVolume.cs
+++ BombsAway/BombsAway/Assets/_Scripts/Audio/SetVolume.cs
@@ -9,13 +9,33 @@ public class SetVolume : MonoBehaviour
     public AudioMixer mixer;
     public Slider slider;
 
+    private AudioManager audioManager;
+    private float currentSliderVal;
+
     private void Start()
     {
         slider.value = PlayerPrefs.GetFloat("MasterVolume", 0.5f);
+        currentSliderVal = slider.value;
+        audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
     }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            //Debug.Log("Clicked");
+            if (currentSliderVal != slider.value)
+            {
+                audioManager.PlayShortClank();
+                currentSliderVal = slider.value;
+            }
+        }
+    }
+
     public void SetLevel()
     {
         float sliderValue = slider.value;
+        Debug.Log(Mathf.Log10(sliderValue));
         if (sliderValue > 0)
         {
             mixer.SetFloat("MasterSound", Mathf.Log10(sliderValue) * 60);
@@ -25,6 +45,5 @@ public class SetVolume : MonoBehaviour
             mixer.SetFloat("MasterSound", -80.0f);
         }
         PlayerPrefs.SetFloat("MasterVolume", sliderValue);
-
     }
 }
