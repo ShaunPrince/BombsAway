e091ac380193fd44ea0bb2ef34a3316c9244d658
Victoria B <vbarinova97@gmail.com>
2020-03-02 15:47:23 -0800

    Minor menu updates, volume slider animated, hold Shift to see control instructions on station

---
+++ BombsAway/BombsAway/Assets/_Scripts/UI/HoldShiftforHints.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldShiftforHints : MonoBehaviour
+{
+    public GameObject[] HintList;
+    private int listlen;
+    void Start()
+    {
+        for (int i = 0; i < HintList.Length; i++) {
+            HintList[i].SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            for (int i = 0; i < HintList.Length; i++)
+            {
+                HintList[i].SetActive(true);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < HintList.Length; i++)
+            {
+                HintList[i].SetActive(false);
+            }
+        }
+    }
+}
